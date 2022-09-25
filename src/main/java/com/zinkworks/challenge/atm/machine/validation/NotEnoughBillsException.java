663a5a3f87a850df4e1044165158e8ef410034f7
gabsw <ana.santos@talkdesk.com>
2022-09-25 11:45:06 +0100

    Changes NotEnoughBillsException to 409

--- src/main/java/com/zinkworks/challenge/atm/machine/validation/NotEnoughBillsException.java
+++ src/main/java/com/zinkworks/challenge/atm/machine/validation/NotEnoughBillsException.java
@@ -3,8 +3,7 @@ package com.zinkworks.challenge.atm.machine.validation;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-// TODO: Internal Server Error
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class NotEnoughBillsException extends Exception {
 
     public NotEnoughBillsException(final String message) {
